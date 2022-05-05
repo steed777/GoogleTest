ffc0537ff9c58db179df98e5730ced353a78e6b4
Senko Konstantin <steed777@yandex.ru>
2022-05-05 14:10:35 +0300

    обновление

--- src/test/java/com/github/steed777/steps/GoogleStep.java
+++ src/test/java/com/github/steed777/steps/GoogleStep.java
@@ -4,17 +4,19 @@ package com.github.steed777.steps;
 
 import io.cucumber.java.en.When;
 import com.github.steed777.flow.GoogleFlow;
+import io.qameta.allure.Step;
 
 
 public class GoogleStep {
 
-
+    @Step("Go to the testing page")
     @When("Go to the testing page")
+
     public void pageAuthorization() {
         GoogleFlow.goGooglePage();
 
     }
-
+    @Step("Entering in the search field 'buy a bork c804 coffee machine' click find")
     @When("Entering in the search field 'buy a bork c804 coffee machine' click find")
     public void authorizationUser() {
         GoogleFlow.enterNameMachine();
